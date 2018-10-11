68c814e7fd2003b6b6090a6fdaba467ec515404e
cody budge <codybudge@gmail.com>
2018-10-11 14:49:37 -0600

    finished consts in snake.js

--- snake.js
+++ snake.js
@@ -20,13 +20,42 @@ const validMove = move => state =>
 const nextMoves = state => state.moves.length > 1 ? dropFirst(state.moves) : state.moves
 const nextApple = state => willEat(state) ? rndPos(state) : state.apple
 const nextHead = state => state.snake.length == 0
-? { x:2, y:2 }
-: {
-  x: mod(state.cols)(state.snake[0].x + state.moves[0].x),
-  y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
-}
+  ? { x: 2, y: 2 }
+  : {
+    x: mod(state.cols)(state.snake[0].x + state.moves[0].x),
+    y: mod(state.rows)(state.snake[0].y + state.moves[0].y)
+  }
 const nextSnake = state => willCrash(state)
-? []
-: (willEat(state)
-? [nextHead(state)].concat(state.snake)
-: [nextHead(state)].concat(dropLast(state.snake)))
+  ? []
+  : (willEat(state)
+    ? [nextHead(state)].concat(state.snake)
+    : [nextHead(state)].concat(dropLast(state.snake)))
+
+//RandomPos
+const rndPos = table => ({
+  x: rnd(0)(table.cols - 1),
+  y: rnd(0)(table.rows - 1)
+})
+
+//Initial State
+const initialState = () => ({
+  cols: 20,
+  rows: 14,
+  moves: [EAST],
+  snake: [],
+  apple: { x: 16, y: 2 }
+})
+
+const next = spec({
+  rows: prop('rows'),
+  cols: prompt('cols'),
+  moves: nextMoves,
+  snake: nextSnake,
+  apple: nextApple
+})
+
+const enqueue = (state, move) => validMove(move)(state)
+  ? merge(state)({ moves: state, moves.concat([move]) })
+  : state
+
+module.exports = { EAST, NORTH, SOUTH, WEST, initialState, enqueue, next, }
